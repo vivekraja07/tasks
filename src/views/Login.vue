ac3edd08636d20d96a791134e481eb75bb31ffb1
vivekraja07 <vivekraja07@gmail.com>
2019-02-16 16:56:43 -0500

    sign up and login pages are added

---
+++ src/views/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="login">
+    <h3> Sign in </h3>
+    <input type="text" placeholder="Email"><br>
+    <input type="password" placeholder="Password"><br>
+    <button @click="login"> Connection </button>
+    <p> You don't have an account ? You can <router-link to="/sign-up"> create one </router-link></p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'login',
+  date() {
+      return {
+
+      };
+  },
+  methods: {
+    login: function() {
+      this.$router.replace('home');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login {
+  margin-top: 40px;
+}
+
+input {
+  margin: 10px 0;
+  width: 20%;
+  padding: 15px;
+}
+
+button {
+  margin-top: 20px;
+  width: 10%;
+  cursor: pointer;
+}
+
+p {
+  margin-top: 40px;
+  font-size: 13px;
+}
+
+p a {
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="sign-up">
+    <h3> Let's Create a New Account !</h3>
+    <input type="text" placeholder="Email"><br>
+    <input type="password" placeholder="Password"><br>
+    <button> Sign Up </button>
+    <span> or go back to <router-link to="/login"> login </router-link> </span>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'signup',
+  date() {
+      return {
+
+      };
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+.signup {
+  margin-top: 40px;
+}
+
+input {
+  margin: 10px 0;
+  width: 20%;
+  padding: 15px;
+}
+
+button {
+  margin-top: 20px;
+  width: 10%;
+  cursor: pointer;
+}
+
+span {
+    display: block;
+    margin-top: 20px;
+    font-size: 11px;
+}
+
+</style>
