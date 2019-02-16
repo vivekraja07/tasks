5356da8acc958db209fe2ddd7ea3e6318d4dbb29
vivekraja07 <vivekraja07@gmail.com>
2019-02-16 16:30:08 -0500

    router added via vue add router

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Table msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Table from '@/components/Table.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Table
+  }
+}
+</script>
